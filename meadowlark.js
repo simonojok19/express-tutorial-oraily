3928e3a5deb7ff696a485a649a440050f2653b60
Simon Peter Ojok <simonojok19@gmail.com>
2019-11-05 11:40:01 +0300

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+const path = require('path');
+
+const app = express();
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+  ];
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortunes: randomFortune});
+});
+
+// custom 404 page
+app.use((req, res) => {
+  res.status(404);
+  res.render('404');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Express started on http://localhost:${app.get('port')} ; press Ctrl-C to terminate`);
+});
